96c21646e079605d334c2655b131d07c823257e5
omoboyejohn-dev <omoboyejohn@gmail.com>
2026-06-30 22:10:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,12 +58,14 @@ function goToCategory() {
 // Dark / Light Mode
 const themeToggle = document.getElementById("themeToggle");
 
-themeToggle.addEventListener("click", function () {
-    document.body.classList.toggle("light-mode");
+if (themeToggle) {
+    themeToggle.addEventListener("click", function () {
+        document.body.classList.toggle("light-mode");
 
-    if (document.body.classList.contains("light-mode")) {
-        themeToggle.innerHTML = "🌞 Light Mode";
-    } else {
-        themeToggle.innerHTML = "🌙 Dark Mode";
-    }
-});
+        if (document.body.classList.contains("light-mode")) {
+            themeToggle.innerHTML = "🌞 Light Mode";
+        } else {
+            themeToggle.innerHTML = "🌙 Dark Mode";
+        }
+    });
+}
